d3a6ee9c2f948326ec581c9dabfd6975ef9a1915
Lan Ejiri <lan.ejiri@yahoo.com>
2018-03-29 10:11:23 -0400

    wah

--- javascript.js
+++ javascript.js
@@ -1,31 +1,49 @@
 $(document).ready(function () {
-    // //initializing firebase
-    // var config = {
-    //     apiKey: "AIzaSyCOSrisKETIrxgYarJC6dGdyZvY-8nAlT8",
-    //     authDomain: "train-time-hw-le.firebaseapp.com",
-    //     databaseURL: "https://train-time-hw-le.firebaseio.com",
-    //     projectId: "train-time-hw-le",
-    //     storageBucket: "",
-    //     messagingSenderId: "207781423415"
-    // };
-    // firebase.initializeApp(config);
+    //initializing firebase
+    var config = {
+        apiKey: "AIzaSyCOSrisKETIrxgYarJC6dGdyZvY-8nAlT8",
+        authDomain: "train-time-hw-le.firebaseapp.com",
+        databaseURL: "https://train-time-hw-le.firebaseio.com",
+        projectId: "train-time-hw-le",
+        storageBucket: "",
+        messagingSenderId: "207781423415"
+    };
+    firebase.initializeApp(config);
 
-    // //defining databse
-    // var database = firebase.database();
+    //defining databse
+    var database = firebase.database();
 
-    var initialTrainData = [
+    var trainData = [
         {
             trainName: "Trenton Express",
             destination: "Trenton",
             frequency: 25,
-            // nextArrival: HH:mm,
+            nextArrival: "17:35",
+        },
+        {
+            trainName: "Oregon Trail",
+            destination: "Salem, Oregon",
+            frequency: 3600,
+            nextArrival: "13:39",
+        },
+        {
+            trainName: "Midnight Carriage",
+            destination: "Philadelphia",
+            frequency: 15,
+            nextArrival: "17:35",
+        },
+        {
+            trainName: "Sing Sing Caravan",
+            destination: "Atlanta",
+            frequency: 45,
+            nextArrival: "17:53",
         }
 
     ];
 
     //console.log(initialTrainData);
 
-    $("#submit-input").on("click",function() {
+    $("#submit-input").on("click", function () {
 
         event.preventDefault();
         var trainNameInput = $("#train-name-input").val();
@@ -33,26 +51,41 @@ $(document).ready(function () {
         var freqInput = parseInt($("#frequency-input").val().trim());
         var firstTrainInput = $("#first-train-input").val().trim();
         console.log(trainNameInput);
-        // var newTrain = {
-        //     trainName: trainNameInput,
-        //     destination: destInput,
-        //     frequency: freqInput,
-        //     nextArrival: firstTrainInput
-        // };
 
-        // var newTrainData = initialTrainData.push(newTrain);
+        var newTrain = {
+            trainName: trainNameInput,
+            destination: destInput,
+            frequency: freqInput,
+            nextArrival: firstTrainInput
+        };
 
-        // console.log(newTrainData);
-
-        //$("#tbody").
+        var newTrainData = trainData.push(newTrain);
 
 
     }); //closing submit button on click function
 
+    function displayTable() {
+        //clear it out first
+        $("#tbody").empty();
+
+        for (i = 0; i < trainData.length; i++) {
+            var tRow = $("<tr>");
+
+            var trainTd = $("<td>").text(trainData[i].trainName);
+            var destTd = $("<td>").text(trainData[i].destination);
+            var freqTd = $("<td>").text(trainData[i].frequency);
+            var nextTd = $("<td>").text(trainData[i].nextArrival);
+
+            tRow.append(trainTd, destTd, freqTd, nextTd);
+            $("#tbody").append(tRow);
+
+        }
 
 
+    }//closing displaytable
 
 
+displayTable();
 
 
 }); //document ready closing
